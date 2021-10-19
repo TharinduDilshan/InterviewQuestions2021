2a48ccb6fecd3a3e368f6d95a468708834ade605
Tatiane Corrêa <43679292+taticorrea@users.noreply.github.com>
2021-10-19 18:04:54 -0300

    remove some typos

--- Python/platescale.py
+++ Python/platescale.py
@@ -1,7 +1,7 @@
 '''
 Program to calculate a plate scale of a Charge-Couple-Device (CCD).
 Input: RA/DEC and ROW/COL coordinates in degrees and pixels for 2 diferent sources
-Output: plate scale in arcsec/pixel and plate scale in arcs/mm
+Output: plate scale in arcsec/pixel and plate scale in arcsec/mm
 '''
 
 import math
@@ -15,19 +15,16 @@ ra = (RA_1 - RA_2)*math.cos(math.radians(avg_DEC))*3600
 dec = (DEC_1 - DEC_2)*3600
 distance_RADEC = math.sqrt(ra**2 + dec**2) #distance between sources in arcsec
 
-
-COL_1 = 1295.92
-COL_2 = 1457.50
-ROW_1 = 819.69
-ROW_2 = 787.51
+COL_1 = 1295.92  #COL coord of a first source in pixel
+COL_2 = 1457.50 #COL coord of a second source in pixel
+ROW_1 = 819.69 #ROW coord of a first source in pixel
+ROW_2 = 787.51 #ROW coord of a second source in pixel
 col = (COL_1 - COL_2)
 row = (ROW_1 - ROW_2)
 distance_PIXEL = math.sqrt(col**2 + row**2) #distance between sources in pixel
 
-
 ps = distance_RADEC/distance_PIXEL #platescale in arcsec/pixel
 ps_mm = ps/0.0135 #platescale in arcs/mm
-
 
 print('plate scale:', ps, 'arcs/pixel\n')
 print('plate scale:', ps_mm, 'arcs/mm')
