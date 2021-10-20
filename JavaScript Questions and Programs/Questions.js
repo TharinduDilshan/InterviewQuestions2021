5e01d1296ef0d794423342aa0c2151984c68d9fa
Anilect Jose <59383190+anilectjose@users.noreply.github.com>
2021-10-20 12:45:54 +0530

    JavaScript Qns

---
+++ JavaScript Questions and Programs/Questions.js
@@ -0,0 +1,72 @@
+Some Important JS Questions 
+
+
+1) What is JavaScript?
+JavaScript is a scripting language. It is different from Java language. It is object-based, lightweight, cross-platform translated language. 
+It is widely used for client-side validation. The JavaScript Translator (embedded in the browser) is responsible for translating the JavaScript code for the web browser.
+
+2) List some features of JavaScript.
+Some of the features of JavaScript are:
+Lightweight
+Interpreted programming language
+Good for the applications which are network-centric
+Complementary to Java
+Complementary to HTML
+Open source
+Cross-platform
+
+3) Who developed JavaScript, and what was the first name of JavaScript?
+JavaScript was developed by Brendan Eich, who was a Netscape programmer. Brendan Eich developed this new scripting language in just ten days in the year September 1995.
+At the time of its launch, JavaScript was initially called Mocha. After that, it was called Live Script and later known as JavaScript.
+
+4) List some of the advantages of JavaScript.
+Some of the advantages of JavaScript are:
+Server interaction is less
+Feedback to the visitors is immediate
+Interactivity is high
+Interfaces are richer
+
+5) List some of the disadvantages of JavaScript.
+Some of the disadvantages of JavaScript are:
+No support for multithreading
+No support for multiprocessing
+Reading and writing of files is not allowed
+No support for networking applications.
+
+6) Define a named function in JavaScript.
+The function which has named at the time of definition is called a named function. For example
+
+function msg()  
+{  
+  document.writeln("Named Function");  
+}  
+msg();  
+
+
+
+7) Name the types of functions
+The types of function are:
+
+Named - These type of functions contains name at the time of definition. For Example:
+function display()  
+{  
+  document.writeln("Named Function");  
+}  
+display();  
+var display=function()  
+{  
+  document.writeln("Anonymous Function");  
+}  
+display();  
+
+
+
+8) Can an anonymous function be assigned to a variable?
+Yes, you can assign an anonymous function to a variable.
+
+9) In JavaScript what is an argument object?
+The variables of JavaScript represent the arguments that are passed to a function.
+
+10) What is the difference between JavaScript and JScript?
+Netscape provided the JavaScript language. Microsoft changed the name and called it JScript to avoid the trademark issue. 
+In other words, you can say JScript is the same as JavaScript, but Microsoft provides it.
