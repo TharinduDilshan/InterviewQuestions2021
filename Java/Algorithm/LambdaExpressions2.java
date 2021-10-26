9bfdac5c7d943ca4037cde2b42a376fde0731485
Umeshika Thathsarani Mahaliyadda <utmahaliyadda@gmail.com>
2021-10-26 17:17:02 +0530

    Added another Lambda Expression example

---
+++ Java/Algorithm/LambdaExpressions2.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class LambdaExpressions2 {
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    numbers.add(5);
+    numbers.add(9);
+    numbers.add(8);
+    numbers.add(1);
+    Consumer<Integer> method = (n) -> { System.out.println(n); };
+    numbers.forEach( method );
+  }
+}
